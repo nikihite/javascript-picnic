d0f1fc0ca32c01765c70ce8af1cc348c952fe7cc
Niki Hite <nikihite@comcast.net>
2022-04-27 11:40:46 -0700

    cheese added

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var sodaItem = document.getElementById('soda');
+var cheeseItem = document.getElementById('cheese');
 
 sodaItem.addEventListener('click', () => {
     sodaItem.classList.toggle('picked');
+});
+
+cheeseItem.addEventListener('click', () => {
+    cheeseItem.classList.toggle('picked');
 });
